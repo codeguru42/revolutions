10e074147ead8610ec9cc0feb1c5a77773b03911
Code Guru <codeguru42@gmail.com>
2014-08-13 15:30:36 -0600

    Initial  libgdx project

---
+++ desktop/src/codeguru/revolutions/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package codeguru.revolutions.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import codeguru.revolutions.RevolutionsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RevolutionsGame(), config);
+	}
+}
